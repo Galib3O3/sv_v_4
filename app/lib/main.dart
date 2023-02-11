df227f4d135033c7fcaf66380c6d5fe5ce214d2b
Galib3O3 <alglaibxyz@gmail.com>
2023-02-11 20:35:35 +0600

    initial commit

---
+++ app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:app/forgate_password/forgatePassword.dart';
+import 'package:app/login_reg/registration.dart';
+import 'package:app/pages/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding();
+  await Firebase.initializeApp();
+  //print(FirebaseAuth.instance.currentUser!);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of my application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Save Community',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: splashS());
+  }
+}
+ //hello man
